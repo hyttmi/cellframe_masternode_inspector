3d238690c6f6973241297830adf058a558273f5a
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-09-18 18:37:04 +0300

    ...

--- response_helpers.py
+++ response_helpers.py
@@ -1,8 +1,7 @@
 import json
+import gzip
 from pycfhelpers.node.http.simple import CFSimpleHTTPResponse
 from logconfig import logger
-import json
-import gzip
 from utils import utils
 from config import Config
 
@@ -14,39 +13,40 @@ class ResponseHelpers:
         "Access-Control-Allow-Headers": "Content-Type, Accept-Encoding",
     }
 
-    if Config.GZIP_RESPONSES:
-        DEFAULT_HEADERS["Content-Encoding"] = "gzip"
-
     @staticmethod
-    def _encode_body(data, gzip_enabled=Config.GZIP_RESPONSES):
+    def _encode_body(data, gzip_enabled=None):
+        gzip_enabled = Config.GZIP_RESPONSES
+
         body = json.dumps(data).encode()
         headers = dict(ResponseHelpers.DEFAULT_HEADERS)
+
         if gzip_enabled:
             body = gzip.compress(body)
             headers["Content-Encoding"] = "gzip"
+
         return body, headers
 
     @staticmethod
-    def success(data, code=200, gzip_enabled=False):
+    def success(data, code=200, gzip_enabled=None):
         body, headers = ResponseHelpers._encode_body(
-            {"request_timestamp": utils.now_iso(), "status": "ok", "data": data}, gzip_enabled
+            {"request_timestamp": utils.now_iso(), "status": "ok", "data": data},
+            gzip_enabled
         )
         logger.debug(f"Response body size: {len(body)} bytes")
         logger.debug(f"Response headers: {headers}")
         return CFSimpleHTTPResponse(body=body, code=code, headers=headers)
 
     @staticmethod
-    def error(message, code=400, gzip_enabled=False):
+    def error(message, code=400, gzip_enabled=None):
         body, headers = ResponseHelpers._encode_body(
-            {"request_timestamp": utils.now_iso(), "status": "error", "message": message}, gzip_enabled
+            {"request_timestamp": utils.now_iso(), "status": "error", "message": message},
+            gzip_enabled
         )
         return CFSimpleHTTPResponse(body=body, code=code, headers=headers)
 
     @staticmethod
     def redirect(url, code=302):
         logger.debug(f"Redirecting to URL: {url}, code: {code}")
-        return CFSimpleHTTPResponse(
-            body=b"",
-            code=code,
-            headers={**ResponseHelpers.DEFAULT_HEADERS, "Location": url},
-        )
+        headers = dict(ResponseHelpers.DEFAULT_HEADERS)
+        headers["Location"] = url
+        return CFSimpleHTTPResponse(body=b"", code=code, headers=headers)
