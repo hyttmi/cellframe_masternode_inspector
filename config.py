55c33dcc46f726f95c8e6bacab8270fd0dfc1f7d
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-10-06 17:14:17 +0300

    Autoupdate == True

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ def get_config_value(section, key, default=None):
 class Config:
     MIN_NODE_VERSION = "5.5.0"
     SUPPORTED_PLATFORMS = ["Linux"]
-    AUTOUPDATE = bool(get_config_value("mninspector", "autoupdate", False))
+    AUTOUPDATE = bool(get_config_value("mninspector", "autoupdate", True))
     ACCESS_TOKEN_ENTROPY = int(get_config_value("mninspector", "access_token_entropy", 64))
     BLOCK_COUNT_THRESHOLD = int(get_config_value("mninspector", "block_count_threshold", 30))
     DAYS_CUTOFF = int(get_config_value("mninspector", "days_cutoff", 20)) # Days
